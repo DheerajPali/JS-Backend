a63aa688c00a528dd4d547220e88c84b1315f9f6
Dheeraj Pali <dheerajpali761@gmail.com>
2024-06-07 14:01:41 +0530

    handled CORS  , and created MIDDLEWARE using express.

--- src/app.js
+++ src/app.js
@@ -4,4 +4,25 @@ import cookieParser from "cookie-parser";
 
 const app = express();
 
+//here we're setting our origin for middleware.
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials : true,
+}))
+
+//here we're setting limits to accept json. 
+app.use(express.json({limit: "10kb"}))
+
+//it will accept urls now. will fetch data from url after encoded it.
+//  some websites use + instead of space but some web.uses %20 , so url type may be change, that's why we're using this to encode it.
+app.use(express.urlencoded({extended:true, limit: "10kb"}))
+
+//we're reffering to a folder("public") which holds static assets(data) , like image etc.
+app.use(express.static("public"))
+
+//it takes cookie , only server can read or modify or remove it.
+//we can perform crud operation on it , for our security purpose.
+app.use(cookieParser())
+
+
 export {app}
