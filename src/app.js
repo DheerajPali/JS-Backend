ed248e794b43616f2d3455cc89a99785b0f20fae
Dheeraj Pali <dheerajpali761@gmail.com>
2024-06-18 17:17:05 +0530

    router import in app.js

--- src/app.js
+++ src/app.js
@@ -1,28 +1,42 @@
-import express from "express"
-import cors from "cors"
+import express from "express";
+import cors from "cors";
 import cookieParser from "cookie-parser";
 
 const app = express();
 
 //here we're setting our origin for middleware.
-app.use(cors({
+app.use(
+  cors({
     origin: process.env.CORS_ORIGIN,
-    credentials : true,
-}))
+    credentials: true,
+  })
+);
 
-//here we're setting limits to accept json. 
-app.use(express.json({limit: "10kb"}))
+//here we're setting limits to accept json.
+app.use(express.json({ limit: "10kb" }));
 
 //it will accept urls now. will fetch data from url after encoded it.
 //  some websites use + instead of space but some web.uses %20 , so url type may be change, that's why we're using this to encode it.
-app.use(express.urlencoded({extended:true, limit: "10kb"}))
+app.use(express.urlencoded({ extended: true, limit: "10kb" }));
 
 //we're reffering to a folder("public") which holds static assets(data) , like image etc.
-app.use(express.static("public"))
+app.use(express.static("public"));
 
 //it takes cookie , only server can read or modify or remove it.
 //we can perform crud operation on it , for our security purpose.
-app.use(cookieParser())
+app.use(cookieParser());
 
+//routes import
 
-export {app}
+// import router from './routes/user.routes.js'
+import userRouter from "./routes/user.routes.js";
+//We can import it with any name because it's exporting by default...
+// import router as userRouter from './routes/user.routes.js'
+
+//router declaration
+/*here we're using .use() instead of .get() means we are calling middleware
+here userRouter will get control , will go to user.router file and call declared method.
+we'll give api ,then it's version then our route.*/
+app.use("/api/v1/users", userRouter);
+
+export { app };
