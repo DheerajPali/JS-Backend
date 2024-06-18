64288162ad79c26c49af9e13ef2f349d367224f6
Dheeraj Pali <dheerajpali761@gmail.com>
2024-06-18 12:49:37 +0530

    Cloudinary initial Configuration

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,40 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+// Configuration
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET, // Click 'View Credentials' below to copy your API secret
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) {
+      console.log("cloudinary : localFilePath not found");
+      return null;
+    }
+
+    //upload the file of cloudinary;
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      //here you can ask for specific file like audio, video,image etc.
+      resource_type: "auto",
+    });
+
+    //file has been uploaded successfully
+    console.log(
+      "file is uploaded on cloudinary, file url : ",
+      response.url,
+      "file info : ",
+      response
+    );
+    return response;
+  } catch (error) {
+    // remove the locally saved temperory file as the upload operation got failed.
+    console.log("cloudinary :: Error : ", error);
+    fs.unlink(localFilePath);
+    return null;
+  }
+};
+
+export { uploadOnCloudinary };
