4d42882279412f67439976d1f9c502e189546762
Dheeraj Pali <dheerajpali761@gmail.com>
2024-06-19 17:03:13 +0530

    used middleware in user.routes.

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,10 +1,27 @@
 import { Router } from "express";
 import { registerUser } from "../controllers/user.controller.js";
 
+import {upload} from "../middlewares/multer.middleware.js";
+
 const router = Router();
 
 //this method will be called after a middleware will give control to this route.
-router.route("/register").post(registerUser);
+router.route("/register").post(
+    //here we are using middleware befor executing "registerUser"
+    upload.fields([
+        {
+            name : "avatar",
+            maxCount : 1
+        },
+        {
+            name : "coverImage",
+            maxCount : 1
+        }
+    ]),
+    registerUser
+);
 
 export default router;
+
+
  
