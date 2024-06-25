5b2a5a0e3f72c170c460fd7361eefff7c9932b7b
Dheeraj Pali <dheerajpali761@gmail.com>
2024-06-25 18:45:12 +0530

    routes added  accorcing to user-controller methods

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,17 @@
 import { Router } from "express";
-import { loginUser, logoutUser, registerUser ,refreshAccessToken} from "../controllers/user.controller.js";
+import { 
+    loginUser, 
+    logoutUser, 
+    registerUser ,
+    refreshAccessToken, 
+    changeCurrentPassword, 
+    getCurrentUser, 
+    updateAccountDetails, 
+    updateUserAvatar, 
+    updateUserCoverImage, 
+    getUserChannelProfile, 
+    getWatchHistory
+}  from "../controllers/user.controller.js";
 
 import {upload} from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
@@ -26,8 +38,24 @@ router.route("/login").post(loginUser)
 
 //secured routes   -means asked data in these methods are already checked, 100% data is available
 router.route("/logout").post(verifyJWT, logoutUser);
-router.route("/refresh-token").post(refreshAccessToken)
+router.route("/refresh-token").post(refreshAccessToken);
 
+router.route("/change-password").post(verifyJWT,changeCurrentPassword);
+
+//here we're not sending anything , so we'll make it get request. 
+router.route("/current-user").get(verifyJWT,getCurrentUser)
+
+//here we're updating some details only , that's why we'll use patch request.
+router.route("/update-account").patch(verifyJWT,updateAccountDetails);
+
+router.route("/update-avatar").patch(verifyJWT,upload.single("avatar"), updateUserAvatar);
+
+router.route("/update-coverImage").patch(verifyJWT,upload.single("coverImage"),updateUserCoverImage)
+
+//here we're getting value from params instead of body
+router.route("/c/:username").get(verifyJWT,getUserChannelProfile)
+
+router.route("/watch-history").get (verifyJWT , getWatchHistory)
 
 export default router;
 
